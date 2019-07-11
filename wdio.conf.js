173592dfafeb9d0bf62dd5acfb4afbba779117ed
minja work <minja.marcetic.work@gmail.com>
2019-07-11 23:19:26 +0200

    Add TimeLine Reporter

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,4 +1,6 @@
 let timeout = process.env.DEBUG ? 99999999 : 180000;
+const { TimelineService } = require('wdio-timeline-reporter/timeline-service');
+
 
 const seleniumOptions = {
      drivers: {
@@ -11,7 +13,6 @@ const seleniumOptions = {
       }
     }
  };
-
 
 
 exports.config = {
@@ -130,7 +131,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: ['chromedriver',[TimelineService]],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
@@ -148,7 +149,16 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: ['spec','dot'],
+    reporters: ['spec','dot',['timeline', {
+          'outputDir' : './reports/',
+          'fileName' : 'timeline-report.html',
+          'embedImages' : true,
+          'images': {
+            quality: 80,
+            resize:false
+          },
+          'screenshotStrategy':'on:error'
+    }]],
 
     //
     // Options to be passed to Mocha.
